c371f2fa39d1d9b3506b73b547cd2b52c8446db3
zhoxin925 <2013268994>
2018-12-26 23:04:21 +0800

    arc

--- app/src/main/java/com/example/temp123/surfaceviewstudy/view/TSurfaceView.java
+++ app/src/main/java/com/example/temp123/surfaceviewstudy/view/TSurfaceView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -30,6 +32,7 @@ public class TSurfaceView extends SurfaceView implements SurfaceHolder.Callback,
 
     private int startAngle;
     private int sweepAngle = 360/8;
+    private int tempAngle;
     private int arcCount = 8;
     private int[] colors = new int[]{Color.BLUE, Color.YELLOW, Color.GREEN, Color.GRAY
             , Color.BLUE, Color.YELLOW, Color.GREEN, Color.GRAY};
@@ -47,6 +50,10 @@ public class TSurfaceView extends SurfaceView implements SurfaceHolder.Callback,
         mHolder = getHolder();
         mHolder.addCallback(this);
 
+//        setZOrderOnTop(true);
+//        mHolder.setFormat(PixelFormat.TRANSLUCENT);
+//        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
         //设置可获得焦点
         setFocusable(true);
         setFocusableInTouchMode(true);
@@ -55,7 +62,7 @@ public class TSurfaceView extends SurfaceView implements SurfaceHolder.Callback,
 
         mShapePaint = new Paint();
         mShapePaint.setAntiAlias(true);
-        mShapePaint.setColor(0xfff1f1f1);
+        mShapePaint.setStrokeWidth(8);
 
         mTextPaint = new Paint();
         mTextPaint.setAntiAlias(true);
@@ -85,7 +92,7 @@ public class TSurfaceView extends SurfaceView implements SurfaceHolder.Callback,
         height = MeasureSpec.getSize(heightMeasureSpec);
         padding = width / 15;
         radius = (width - padding * 2) / 2;
-        arcRectF = new RectF(padding, padding, width, width);
+        arcRectF = new RectF(-radius, -radius, radius, radius);
     }
 
     @Override
@@ -109,21 +116,65 @@ public class TSurfaceView extends SurfaceView implements SurfaceHolder.Callback,
 
     //角度=180°×弧度÷π ，弧度=角度×π÷180°。
     private void drawSomeThing(Canvas canvas) {
+//        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+
+        mShapePaint.setColor(0xfff1f1f1);
         canvas.drawRect(new Rect(padding, (height - radius*2) / 2
                 , padding + radius*2, (height - radius*2) / 2 + radius*2), mShapePaint);
         canvas.translate(width/2, height/2);
 
+        mShapePaint.setStyle(Paint.Style.FILL);
+        startAngle = 0;
+        tempAngle = 0;
+//        for(int i=0; i<arcCount; i++) {
+//            mShapePaint.setColor(colors[1]);
+//            tempAngle += sweepAngle;
+//            canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+//            startAngle = tempAngle;
+//        }
+
+        startAngle = 0;
+        tempAngle = sweepAngle;
+        mShapePaint.setColor(colors[0]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[1]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[2]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[3]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[4]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[5]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[6]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
+        startAngle = tempAngle;
+        tempAngle += sweepAngle;
+        mShapePaint.setColor(colors[7]);
+        canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
+
         mShapePaint.setColor(Color.RED);
-        mShapePaint.setStrokeWidth(8);
         mShapePaint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(0, 0, radius, mShapePaint);
-
-        for(int i=0; i<arcCount; i++) {
-            mShapePaint.setColor(colors[i]);
-            canvas.drawArc(arcRectF, startAngle, sweepAngle, true, mShapePaint);
-            sweepAngle += sweepAngle;
-        }
-
-
     }
 }
