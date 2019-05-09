115e457070615d04e644876f2b083e000bc6bad4
zhoxin925 <2013268994@qq.com>
2019-05-09 17:47:53 +0800

    Android消息机制

--- app/src/main/java/com/example/temp123/surfaceviewstudy/MainActivity.java
+++ app/src/main/java/com/example/temp123/surfaceviewstudy/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.temp123.surfaceviewstudy;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.d("handler study---what==", String.valueOf(msg.what));
+            Log.d("handler study---obj==", (String) msg.obj);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Message msg = Message.obtain();
+        msg.what = 1;
+        msg.obj = "hello handler";
+        mHandler.sendMessage(msg);
+
+        AsyncTask
+        ThreadLocal<Boolean> threadLocal = new ThreadLocal<>();
+        threadLocal.set(false);
+        threadLocal.get();
+
+        for(;;)
+        {
+            int i=0;
+            if(i==10){
+               return;
+            }
+            i++;
+        }
     }
 }
